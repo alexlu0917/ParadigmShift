39b4a9e20d784f28fc0d192860696cafbcd614c1
alexlu0917 <alexlu0917.dev@gmail.com>
2022-03-16 02:06:00 +0000

    create AxleToken smart contract

--- contracts/AxleToken.sol
+++ contracts/AxleToken.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol";
 
-contract AxleToken {
-    
+contract AxleToken is ERC20PresetMinterPauser {    
+    constructor() ERC20PresetMinterPauser("Axle", "AXL") {}
 }
