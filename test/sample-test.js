7abe19c8e6cfe94f00e412a70079932a173ece12
alexlu0917 <alexlu0917.dev@gmail.com>
2022-03-23 23:39:27 +0000

    update test

--- test/sample-test.js
+++ test/sample-test.js
@@ -35,39 +35,39 @@ describe("TokenSale Test", function () {
   });
 
   it("Should be equal token balance", async function () {
-   await tokenSale.connect(addr1).deposit({from: addr1.address, value: toTransfer});
-    
+    await tokenSale.connect(addr1).deposit({ from: addr1.address, value: toTransfer });
+
     expect(await tokenSale.userBalance(addr1.address)).to.deep.equal(toTransfer);
     expect(await tokenSale.whitelist(addr1.address)).to.equal(true);
   });
 
   it("Twice deposit test", async function () {
-    await tokenSale.connect(addr1).deposit({from: addr1.address, value: toTransfer});
-    await tokenSale.connect(addr2).deposit({from: addr2.address, value: toTransfer});
-    await tokenSale.connect(addr1).deposit({from: addr1.address, value: toTransfer});
-     
-     expect(await tokenSale.userBalance(addr1.address)).to.equal(ethers.utils.parseEther("2"));
-     expect(await tokenSale.userBalance(addr2.address)).to.equal(ethers.utils.parseEther("1"));
-     expect(await tokenSale.whitelist(addr1.address)).to.equal(true);
-     expect(await tokenSale.whitelist(addr2.address)).to.equal(true);
-   });
+    await tokenSale.connect(addr1).deposit({ from: addr1.address, value: toTransfer });
+    await tokenSale.connect(addr2).deposit({ from: addr2.address, value: toTransfer });
+    await tokenSale.connect(addr1).deposit({ from: addr1.address, value: toTransfer });
 
-   it("TokenSale contract balance test", async function () {
-    await tokenSale.connect(addr1).deposit({from: addr1.address, value: toTransfer});
-    await tokenSale.connect(addr2).deposit({from: addr2.address, value: toTransfer});
-    await tokenSale.connect(addr1).deposit({from: addr1.address, value: toTransfer});
+    expect(await tokenSale.userBalance(addr1.address)).to.equal(ethers.utils.parseEther("2"));
+    expect(await tokenSale.userBalance(addr2.address)).to.equal(ethers.utils.parseEther("1"));
+    expect(await tokenSale.whitelist(addr1.address)).to.equal(true);
+    expect(await tokenSale.whitelist(addr2.address)).to.equal(true);
+  });
+
+  it("TokenSale contract balance test", async function () {
+    await tokenSale.connect(addr1).deposit({ from: addr1.address, value: toTransfer });
+    await tokenSale.connect(addr2).deposit({ from: addr2.address, value: toTransfer });
+    await tokenSale.connect(addr1).deposit({ from: addr1.address, value: toTransfer });
 
     expect(await ethers.provider.getBalance(tokenSale.address)).to.equal(ethers.utils.parseEther("3"));
-   });
+  });
 
-   it("Set exchange rate", async function () {
+  it("Set exchange rate", async function () {
     await tokenSale.setExchangeRate(5);
     expect(await tokenSale.exchangeRate()).to.equal(5);
-   });
+  });
 
-   it("Set Stage", async function () {
-    let startTime = new Date('2022-4-1').getTime()/1000;
-    let endTime = new Date('2022-6-30').getTime()/1000;
+  it("Set Stage", async function () {
+    let startTime = new Date('2022-4-1').getTime() / 1000;
+    let endTime = new Date('2022-6-30').getTime() / 1000;
     let exchangeRate = 1000;
     let maxAmount = 1000;
     let minAmount = 10;
@@ -78,5 +78,5 @@ describe("TokenSale Test", function () {
     expect(await tokenSale.endTime()).to.equal(endTime);
     expect(await tokenSale.minAmount()).to.equal(minAmount);
     expect(await tokenSale.maxAmount()).to.equal(maxAmount);
-   });
+  });
 });
